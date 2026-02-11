4dfdbb59e5f013e4747de91a3162310b878e5d87
edingonuler <eding2@nycstudents.net>
2026-02-11 14:01:59 -0500

    f

---
+++ vue-project/src/views/MovieShop.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 style="text-align: center"><u>Movie Shop</u></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {}
+  },
+}
+</script>
+
+<style scoped>
+template {
+  background-color: aliceblue;
+}
+</style>
