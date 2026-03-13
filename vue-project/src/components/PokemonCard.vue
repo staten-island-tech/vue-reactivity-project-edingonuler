3bea5e6e7fe7c51354afd8c4d1e1fa8cfabfb5b4
edingonuler <eding2@nycstudents.net>
2026-03-13 14:00:25 -0400

    f

---
+++ vue-project/src/components/PokemonCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="card">
+    <h2>{{ pokemon.name }}</h2>
+    <h3>{{ id }}</h3>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  pokemon: {
+    type: Object,
+    required: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+
+<style scoped>
+.card {
+  width: 28%;
+  height: 500px;
+  background-color: aliceblue;
+  margin: 30px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  text-transform: uppercase;
+}
+img {
+  width: 100%;
+  height: 80%;
+  object-fit: cover;
+}
+</style>
---
+++ vue-project/src/views/PokemonData.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>Hello Beila Bad, Vincent Good</h2>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const pokemon = ref({})
+async function getPokemon(id) {
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+  const data = await response.json()
+  pokemon.value = data
+}
+onMounted(() => {
+  onMounted(route.params.id)
+})
+watch(
+  () => route.params.id,
+  function (id) {
+    getPokemon(id)
+  },
+)
+</script>
+
+<style scoped></style>
